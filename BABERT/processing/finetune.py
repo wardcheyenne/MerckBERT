b44c48781b6c6dee61577ff7cb66624c1415ddae
Cheyenne Ward <84690878+wardcheyenne@users.noreply.github.com>
2023-01-07 12:56:55 -0800

    Create finetune.py

---
+++ BABERT/processing/finetune.py
@@ -0,0 +1,189 @@
+import data_management
+import BERT_tokenizer
+import numpy as np
+import torch
+import torch.nn.functional as F
+
+from transformers import BertForTokenClassification
+
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+df = data_management.df[0:20]
+df_train, df_val, df_test = np.split(df.sample(frac=1, random_state=42),
+                            [int(.8 * len(df)), int(.9 * len(df))]) # change to .8
+        
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+class BertModel(torch.nn.Module):
+
+    def __init__(self):
+
+        super(BertModel, self).__init__()
+
+        self.bert = BertForTokenClassification.from_pretrained('bert-base-uncased', num_labels=len(data_management.unique_labels))
+
+    def forward(self, input_id, mask, label):
+
+        output = self.bert(input_ids=input_id, attention_mask=mask, labels=label, return_dict=False)
+
+        return output
+        
+def train_loop(model, df_train, df_val):
+
+    train_dataset = DataSequence(df_train)
+    val_dataset = DataSequence(df_val)
+
+    #print(len(train_dataset))
+    #print(len(val_dataset))
+    train_dataloader = DataLoader(train_dataset, num_workers=4, batch_size=BATCH_SIZE, shuffle=True)
+    val_dataloader = DataLoader(val_dataset, num_workers=4, batch_size=BATCH_SIZE)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    optimizer = optim.SGD(model.parameters(), lr=LEARNING_RATE)
+
+    if use_cuda:
+        model = model.cuda()
+
+    best_acc = 0
+    best_loss = 1000
+
+    for epoch_num in range(EPOCHS):
+
+        total_acc_train = 0
+        total_loss_train = 0
+
+        model.train()
+
+        for train_data, train_label in tqdm(train_dataloader):
+
+            train_label = train_label.to(device)
+            mask = train_data['attention_mask'].squeeze(1).to(device)
+            input_id = train_data['input_ids'].squeeze(1).to(device)
+
+            optimizer.zero_grad()
+            loss, logits = model(input_id, mask, train_label)
+
+            for i in range(logits.shape[0]):
+
+              logits_clean = logits[i][train_label[i] != -100]
+              label_clean = train_label[i][train_label[i] != -100]
+
+              predictions = logits_clean.argmax(dim=1)
+              probabilities = F.softmax(logits, dim=-1).tolist()
+              acc = (predictions == label_clean).float().mean()
+              total_acc_train += acc
+              total_loss_train += loss.item()
+
+            loss.backward()
+            optimizer.step()
+		
+        model.eval()
+
+        total_acc_val = 0
+        total_loss_val = 0
+
+        for val_data, val_label in val_dataloader:
+
+            val_label = val_label.to(device)
+            mask = val_data['attention_mask'].squeeze(1).to(device)
+            input_id = val_data['input_ids'].squeeze(1).to(device)
+
+            loss, logits = model(input_id, mask, val_label)
+
+            for i in range(logits.shape[0]):
+
+              logits_clean = logits[i][val_label[i] != -100]
+              label_clean = val_label[i][val_label[i] != -100]
+
+              predictions = logits_clean.argmax(dim=1)
+              acc = (predictions == label_clean).float().mean()
+              total_acc_val += acc
+              total_loss_val += loss.item()
+
+        val_accuracy = total_acc_val / len(df_val)
+        val_loss = total_loss_val / len(df_val)
+
+        print(
+            f'Epochs: {epoch_num + 1} | Loss: {total_loss_train / len(df_train): .3f} | Accuracy: {total_acc_train / len(df_train): .3f} | Val_Loss: {total_loss_val / len(df_val): .3f} | Accuracy: {total_acc_val / len(df_val): .3f}')
+
+LEARNING_RATE = 2e-3
+EPOCHS = 15
+BATCH_SIZE = 2
+
+torch.save(BertModel(), './models/BABERT.pth')
+model = torch.load('./models/BABERT.pth')
+#train_loop(model, df_train, df_val)
+
+def evaluate(model, df_test):
+
+    test_dataset = BERT_tokenizer.DataSequence(df_test)
+
+    test_dataloader = DataLoader(test_dataset, num_workers=4, batch_size=1)
+
+    use_cuda = torch.cuda.is_available()
+    device = torch.device("cuda" if use_cuda else "cpu")
+
+    if use_cuda:
+        model = model.cuda()
+
+    total_acc_test = 0.0
+
+    for test_data, test_label in test_dataloader:
+
+            test_label = test_label.to(device)
+            mask = test_data['attention_mask'].squeeze(1).to(device)
+
+            input_id = test_data['input_ids'].squeeze(1).to(device)
+
+            loss, logits = model(input_id, mask, test_label)
+
+            for i in range(logits.shape[0]):
+
+              logits_clean = logits[i][test_label[i] != -100]
+              label_clean = test_label[i][test_label[i] != -100]
+
+              predictions = logits_clean.argmax(dim=1)
+              probabilities = F.softmax(logits, dim=-1).tolist()
+              acc = (predictions == label_clean).float().mean()
+              total_acc_test += acc
+
+    val_accuracy = total_acc_test / len(df_test)
+   #print(f'Test Accuracy: {total_acc_test / len(df_test): .3f}')
+
+
+evaluate(model, df_test)
+
+def align_word_ids(texts):
+  
+    tokenized_inputs = BERT_tokenizer.tokenizer(texts, padding='max_length', max_length=512, truncation=True)
+
+    word_ids = tokenized_inputs.word_ids()
+
+    previous_word_idx = None
+    label_ids = []
+
+    for word_idx in word_ids:
+
+        if word_idx is None:
+            label_ids.append(-100)
+
+        elif word_idx != previous_word_idx:
+            try:
+                label_ids.append(1)
+            except:
+                label_ids.append(-100)
+        else:
+            try:
+                label_ids.append(1 if label_all_tokens else -100)
+            except:
+                label_ids.append(-100)
+        previous_word_idx = word_idx
+
+    return label_ids
+
